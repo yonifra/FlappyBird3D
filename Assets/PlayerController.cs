76434454d1c8f9d3b72d41f8ca685d89a93fb3ec
Jonathan Fraimorice <jonathanfr@wix.com>
2019-12-16 10:01:53 +0200

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody player;
+    public float forwardSpeed = 1200f;
+    public float upwardForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.AddForce(0, 0, forwardSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            player.AddForce(0, upwardForce * Time.deltaTime, 0);
+        }
+    }
+}
